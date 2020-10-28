d04a56d0b18fe5280c78f320e7ce7f0ae919000f
Joshua Lynch <jlynch@bnl.gov>
2020-10-28 11:40:23 -0500

    instantiate ZMQ publisher outside factory function apply automatic formatting

--- ae_gpcam/bluesky_config/scripts/analysis_consumer.py
+++ ae_gpcam/bluesky_config/scripts/analysis_consumer.py
@@ -6,14 +6,21 @@ from bluesky.callbacks.zmq import Publisher as ZmqPublisher
 from event_model import RunRouter
 
 
+zmq_listening_prefix = b"from-RE"
+
 zmq_dispatcher = ZmqRemoteDispatcher(
-    address=("127.0.0.1", 5678),
-    prefix=b'adaptive'
+    address=("127.0.0.1", 5678), prefix=zmq_listening_prefix
+)
+
+zmq_analysis_publisher = ZmqPublisher(
+    address=("127.0.0.1", 4567), prefix=b"from-analysis"
 )
 
 
 def zmq_publish_from_analysis_factory(start_name, start_doc):
-    zmq_analysis_publisher = ZmqPublisher(address=("127.0.0.1", 4567), prefix=b'from-analysis')
+    print(
+        f"zmq_publish_from_analysis_factory called with {start_name}:\n{pprint.pformat(start_doc)}\n"
+    )
 
     def zmq_publish_from_analysis(name, doc):
         print(f"analysis consumer publishing {name}:\n{pprint.pformat(doc)}\n")
@@ -22,8 +29,7 @@ def zmq_publish_from_analysis_factory(start_name, start_doc):
     return [zmq_publish_from_analysis], []
 
 
-rr = RunRouter(factories=[zmq_publish_from_analysis_factory])
+zmq_dispatcher.subscribe(RunRouter(factories=[zmq_publish_from_analysis_factory]))
 
-zmq_dispatcher.subscribe(rr)
-print("ANALYSIS CONSUMER IS READY TO START")
+print(f"ANALYSIS CONSUMER IS LISTENING ON {zmq_listening_prefix}")
 zmq_dispatcher.start()
