d43e73d2f7af74d11b18437fc2c25845df47fcce
Joshua Lynch <jlynch@bnl.gov>
2020-11-12 13:53:38 -0500

    mount TiCu data directory

--- ae_gpcam/launch_bluesky_headless.sh
+++ ae_gpcam/launch_bluesky_headless.sh
@@ -8,12 +8,14 @@ else
     imagename="bluesky"
 fi
 
+# add -v /vagrant/TiCu_export for sbu_sim and ae-xpd
 podman run --pod acquisition \
        -ti  --rm \
        -v `pwd`:'/app' -w '/app' \
        -v ./bluesky_config/ipython:/usr/local/share/ipython \
        -v ./bluesky_config/databroker:/usr/local/share/intake \
        -v ./bluesky_config/happi:/usr/local/share/happi \
+       -v /vagrant/TiCu_export:/usr/local/share/TiCu_export \
        -e EPICS_CA_ADDR_LIST=10.0.2.255 \
        -e EPICS_CA_AUTO_ADDR_LIST=no \
        $imagename \
