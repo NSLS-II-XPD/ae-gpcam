6e71c00d51399b17e5661d7d8b9d671ee1c4608e
Joshua Lynch <jlynch@bnl.gov>
2020-12-03 21:41:35 -0500

    use the gpcam conda environment

--- ae_gpcam/launchers/start_adaptive_gpcam_server.sh
+++ ae_gpcam/launchers/start_adaptive_gpcam_server.sh
@@ -2,4 +2,4 @@
 set -e
 set -o xtrace
 
-podman run --pod acquisition --rm -ti -v `pwd`/bluesky_config/scripts:'/app' -w '/app' bluesky python3 adaptive_gpcam_consumer.py
+podman run --pod acquisition --rm -ti -v `pwd`/bluesky_config/scripts:'/app' -w '/app' bluesky /root/.conda/envs/gpcam/bin/python adaptive_gpcam_consumer.py
