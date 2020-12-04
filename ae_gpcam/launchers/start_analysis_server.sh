e58feb3b0e790681808aa67dcc1f9caf35facac9
Joshua Lynch <jlynch@bnl.gov>
2020-12-03 21:41:07 -0500

    rename analysis_consumer.py to not_xpdan_consumer.py

--- ae_gpcam/launchers/start_analysis_server.sh
+++ ae_gpcam/launchers/start_analysis_server.sh
@@ -2,4 +2,4 @@
 set -e
 set -o xtrace
 
-podman run --pod acquisition --rm -ti -v `pwd`/bluesky_config/scripts:'/app' -w '/app' bluesky python3 analysis_consumer.py
+podman run --pod acquisition --rm -ti -v `pwd`/bluesky_config/scripts:'/app' -w '/app' bluesky python3 not_xpdan_consumer.py
