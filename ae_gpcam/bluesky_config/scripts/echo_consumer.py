2aaf8d749bd18d25e5a4260698359441d3f50a2d
XPD Operator <xf28id2@xf28id1-ws2.cs.nsls2.local>
2020-12-08 17:45:17 -0500

    print uuids

--- ae_gpcam/bluesky_config/scripts/echo_consumer.py
+++ ae_gpcam/bluesky_config/scripts/echo_consumer.py
@@ -2,14 +2,21 @@ import sys
 
 from bluesky.callbacks.zmq import RemoteDispatcher
 
+
 # listen for 0MQ messages from xf28id2-ca1:5578
 zmq_server = "xf28id2-ca1:5578"
 zmq_prefix = sys.argv[1].encode()
 
 def echo(name, doc):
     print(f"got a {name} document with 0MQ prefix {zmq_prefix}")
-    if "run_start" in doc:
-        print(f"  run_start uid {doc['run_start']}")
+    if name == "start":
+        print(f"  start id {doc['uid']}")
+    elif name == "descriptor":
+        print(f"  start id {doc['run_start']}")
+        print(f"  uid {doc['uid']}")
+    elif name == "event":
+        print(f"  descriptor id {doc['descriptor']}")
+        print(f"  uid {doc['uid']}")
 
 d = RemoteDispatcher("xf28id2-ca1:5578", prefix=zmq_prefix)
 d.subscribe(echo)
