021a3b21c1b1fcb49436a669013adfba88df20ad
Joshua Lynch <jlynch@bnl.gov>
2020-12-08 12:44:16 -0500

    format with black

--- ae_gpcam/bluesky_config/scripts/emit_databroker_documents.py
+++ ae_gpcam/bluesky_config/scripts/emit_databroker_documents.py
@@ -22,32 +22,34 @@ pprint.pprint(vars(args))
 
 db = Broker.named("xpd")
 
-zmq_publisher = Publisher(f"{args.zmq_host}:{args.zmq_publish_port}", prefix=args.zmq_publish_prefix.encode())
+zmq_publisher = Publisher(
+    f"{args.zmq_host}:{args.zmq_publish_port}", prefix=args.zmq_publish_prefix.encode()
+)
 run_uid = "88e450ff-b1a7-4a17-a353-ab40653b7675"
 
 extra = count()
 for name, doc in db[args.run_id].documents():
     print(f"trying to emit {name}")
     doc = dict(doc)
-    if name == 'descriptor':
-        doc['data_keys']['extra'] =  {
-                        "dtype": "number",
-                        "source": "computed",
-                        "units": "arb",
-                        "shape": [],
-                    }
-        zmq_publisher('descriptor', doc)
-    if name =='event_page':
+    if name == "descriptor":
+        doc["data_keys"]["extra"] = {
+            "dtype": "number",
+            "source": "computed",
+            "units": "arb",
+            "shape": [],
+        }
+        zmq_publisher("descriptor", doc)
+    if name == "event_page":
         for ev in unpack_event_page(doc):
             for j in range(5):
                 new_seq = next(extra)
-                ev['seq_num'] = new_seq
-                ev['uid'] = str(uuid.uuid4())
-                ev['data']['extra'] = 5
-                ev['timestamps']['extra'] = 0
-                zmq_publisher('event', ev)
-    elif name == 'datum_page':
+                ev["seq_num"] = new_seq
+                ev["uid"] = str(uuid.uuid4())
+                ev["data"]["extra"] = 5
+                ev["timestamps"]["extra"] = 0
+                zmq_publisher("event", ev)
+    elif name == "datum_page":
         for datum in unpack_datum_page(doc):
-            zmq_publisher('datum', datum)
+            zmq_publisher("datum", datum)
     else:
         zmq_publisher(name, doc)
