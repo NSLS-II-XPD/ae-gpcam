7063b6200651e840e887b56ff04567af4724db85
Thomas A Caswell <tcaswell@bnl.gov>
2020-12-10 16:58:35 -0500

    WIP: attempt to add thickness to the model

--- ae_gpcam/bluesky_config/ipython/adaptive_test.py
+++ ae_gpcam/bluesky_config/ipython/adaptive_test.py
@@ -8,6 +8,7 @@ import bluesky.plans as bp
 from ophyd.sim import SynAxis
 from pprint import pp
 
+
 class BatchLiveTable(LiveTable):
     def start(self, doc):
         if self._start is not None:
@@ -29,7 +30,9 @@ class BatchLiveTable(LiveTable):
             super().descriptor(doc)
 
 
-lt = BatchLiveTable(["ctrl_Ti", "ctrl_temp", "ctrl_anneal_time", "x", "y"])
+lt = BatchLiveTable(
+    ["ctrl_Ti", "ctrl_temp", "ctrl_anneal_time", "ctrl_thickness", "x", "y"]
+)
 
 y = SynAxis(name="y")
 x = SynAxis(name="x")
@@ -38,10 +41,14 @@ x = SynAxis(name="x")
 RE = RunEngine()
 RE.subscribe(lt)
 
-recommender = SequenceRecommender([[30, 340, 450], [35, 340, 450], [35, 400, 450]])
+recommender = SequenceRecommender(
+    [[30, 340, 450, 1], [35, 340, 450, 1], [48, 400, 3600, 1], [35, 400, 450, 0]]
+)
 
 cb, queue = recommender_factory(
-    recommender, ["ctrl_Ti", "ctrl_temp", "ctrl_anneal_time"], ["x"]
+    recommender,
+    ["ctrl_Ti", "ctrl_temp", "ctrl_annealing_time", "ctrl_thickness"],
+    ["x"],
 )
 
 pair = single_strip_set_transform_factory(single_data)
@@ -50,13 +57,13 @@ snap_function = snap_factory(single_data, time_tol=5, temp_tol=10, Ti_tol=None)
 RE(
     adaptive_plan(
         [],
-        (30, 460, 30 * 60),
+        (30, 460, 30 * 60, 1),
         to_recommender=cb,
         from_recommender=queue,
         real_motors=(x, y),
         transform_pair=pair,
         snap_function=snap_function,
-        take_data=stepping_ct
+        take_data=stepping_ct,
     ),
-    lambda name, doc: pp(doc) if name == 'start' else None
+    lambda name, doc: pp(doc) if name == "start" else None,
 )
