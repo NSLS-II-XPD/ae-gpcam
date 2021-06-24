006ffd052ce84f96a4f20b5559ea657a4a6620bc
maffettone <phillip.maffettone@gmail.com>
2021-06-24 08:12:57 -0400

    Retain and record probabilities, POI, and reasoning in both real and scientific space. Add output methods.

--- companion/xca.py
+++ companion/xca.py
@@ -10,46 +10,77 @@ import json
 from bluesky.utils import install_qt_kicker
 from itertools import cycle
 from data_access.acces_grid import single_strip_set_transform_factory, load_from_json
+from dataclasses import dataclass, asdict
 
 
-class XCACompanion():
-    def __init__(self,
-                 model_name='bkg_ideal',
-                 q_range=(2, 4),
-                 transform_path='../data_access/layout.json',
-                 ignore_phases=('Mg',)):
+@dataclass
+class Proposal:
+    phase_of_interest: str
+    x: float
+    y: float
+    sci_coords: list
+    probabilities: list
+
+
+class XCACompanion:
+    def __init__(
+        self,
+        model_name="bkg_ideal",
+        q_range=(2, 4),
+        transform_path="../data_access/layout.json",
+        ignore_phases=("Mg",),
+    ):
 
         self.model_name = model_name
-        model_path = Path('./saved_models/') / model_name
+        model_path = Path("./saved_models/") / model_name
         self.model = tf.keras.models.load_model(str(model_path))
-        self.phasemap = {0: 'MgCu2', 1: 'Mg', 2: 'Ti', 3: 'Mg2Cu'}
-        self.phase_idx = [key for key in self.phasemap if self.phasemap[key] not in ignore_phases]
+        self.phasemap = {0: "MgCu2", 1: "Mg", 2: "Ti", 3: "Mg2Cu"}
+        self.phase_idx = [
+            key for key in self.phasemap if self.phasemap[key] not in ignore_phases
+        ]
         self.strip_infos = load_from_json(transform_path)
         self.strip_transforms = single_strip_set_transform_factory(self.strip_infos)
         self.strip_ys = cycle([strip.reference_y for strip in self.strip_infos])
         self.q_range = q_range
         self.independent = None
         self.dependent = None
-        self.cache = set()
+        self.cache = set()  # Hashable cache of proposals
+        self.proposals = list()  # More data rich list of proposals
 
     def _preprocessing(self, IoQ):
         """Takes array [[Q],[I]] and converts it to relevant Q range for Neural net"""
-        if self.model_name in ('background_lite', 'background', 'bkg_limit_texture', 'bkg_ideal'):
+        if self.model_name in (
+            "background_lite",
+            "background",
+            "bkg_limit_texture",
+            "bkg_ideal",
+        ):
             # Qs = np.array(data["q"])
             Qs = IoQ[:, :, 0]
             Is = IoQ[:, :, 1]
             q_range = self.q_range
-            idx_min = np.where(Qs[0, :] < q_range[0])[0][-1] if len(np.where(Qs[0, :] < q_range[0])[0]) else 0
-            idx_max = np.where(Qs[0, :] > q_range[1])[0][0] if len(np.where(Qs[0, :] > q_range[1])[0]) else Is.shape[1]
+            idx_min = (
+                np.where(Qs[0, :] < q_range[0])[0][-1]
+                if len(np.where(Qs[0, :] < q_range[0])[0])
+                else 0
+            )
+            idx_max = (
+                np.where(Qs[0, :] > q_range[1])[0][0]
+                if len(np.where(Qs[0, :] > q_range[1])[0])
+                else Is.shape[1]
+            )
             Is = Is[:, idx_min:idx_max]
-            I_norm = (Is - np.min(Is, axis=1, keepdims=True)) / \
-                     (np.max(Is, axis=1, keepdims=True) - np.min(Is, axis=1, keepdims=True))
+            I_norm = (Is - np.min(Is, axis=1, keepdims=True)) / (
+                np.max(Is, axis=1, keepdims=True) - np.min(Is, axis=1, keepdims=True)
+            )
             # Dimensions are imporant and TF is picky.
             I_norm = np.reshape(I_norm, (-1, 576, 1))
             return I_norm
 
         else:
-            raise ValueError(f"{self.model_name} is not a known model type for preprocessing")
+            raise ValueError(
+                f"{self.model_name} is not a known model type for preprocessing"
+            )
 
     def predict(self, IoQ):
         # Everything should be conceptualized as batch processing of (576, 1) arrays, even if it is a batch of 1
@@ -79,10 +110,14 @@ class XCACompanion():
         """
         n = min(n, len(self.independent))  # Avoid unecessary looping.
         proposals = []
-        for current_y in self.strip_ys:  # Strip ys is a cycle, so will continue indefinitely
+        for (
+            current_y
+        ) in self.strip_ys:  # Strip ys is a cycle, so will continue indefinitely
             # Get our interesting indexes sorted according to phase where the strip is current
             for phase in self.phase_idx:
-                jdxs = np.argsort(self.dependent[:, phase])[np.abs(self.independent[:, 1] - current_y) < 4.5/2]
+                jdxs = np.argsort(self.dependent[:, phase])[
+                    np.abs(self.independent[:, 1] - current_y) < 4.5 / 2
+                ]
                 for j in jdxs:
                     proposal = self.independent[j, :]
                     if tuple(proposal) in self.cache:
@@ -90,6 +125,15 @@ class XCACompanion():
                     else:
                         self.cache.add(tuple(proposal))
                         proposals.append(self.strip_transforms.inverse(*proposal))
+                        self.proposals.append(
+                            Proposal(
+                                phase_of_interest=self.phasemap[phase],
+                                x=proposal[0],
+                                y=proposal[1],
+                                sci_coords=proposals[-1],
+                                probabilities=self.dependent[j, :],
+                            )
+                        )
                         if len(proposals) >= n:
                             return proposals
                         else:
@@ -142,12 +186,47 @@ class XCACompanion():
             self.dependent = np.vstack([self.dependent, y_preds])
 
 
+def record_output_probabilities(xca, out_path):
+    """Helper function to record XCA probabilities over initial scan to file"""
+    from pandas import DataFrame
+
+    data_dict = {}
+
+    data_dict["Position 0"] = xca.independent[:, 0]
+    data_dict["Position 1"] = xca.independent[:, 1]
+    for idx in xca.phase_idx:
+        data_dict[f"Probability of {xca.phasemap[idx]}"] = xca.dependent[:, idx]
+
+    df = DataFrame.from_dict(data_dict)
+    df.to_csv(out_path)
+    return df
+
+
+def record_detailed_proposals(xca, out_path):
+    from pandas import DataFrame
+
+    dicts = [asdict(p) for p in xca.proposals]
+    df = DataFrame(dicts)
+
+    df[["Ti_frac", "temperature", "annealing_time", "thickness"]] = DataFrame(
+        df["sci_coords"].tolist(), index=df.index
+    )
+    df[[f"Prby {xca.phasemap[i]}" for i in range(4)]] = DataFrame(
+        df["probabilities"].tolist(), index=df.index
+    )
+    del df["probabilities"]
+    del df["sci_coords"]
+    df.to_csv(out_path)
+    return df
+
+
 if __name__ == "__main__":
     from data_access.acces_grid import pre_process
+
     # THIS IS A BAD HACK FOR MAC TESTING #
     import os
 
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
     # THIS IS A BAD HACK FOR MAC TESTING #
 
     import databroker
@@ -157,7 +236,12 @@ if __name__ == "__main__":
     # Creating a pretend document via dictionary
     # Do the work of extract_event_page()
     cat = databroker._drivers.msgpack.BlueskyMsgpackCatalog(
-        str(Path('~/Documents/Project-Adaptive/KarenChenWiegart/kyc_day1/*').expanduser()))
+        str(
+            Path(
+                "~/Documents/Project-Adaptive/KarenChenWiegart/kyc_day1/*"
+            ).expanduser()
+        )
+    )
     for name in cat:
         print(name)
         ds = cat[name].primary.read()
@@ -166,7 +250,7 @@ if __name__ == "__main__":
     independent = np.array(science_pos)  # 4 space
 
     xca.tell_many(independent, measurement)
-    proposals = xca.ask(10)
-    print(proposals)
-    proposals = xca.ask(10)
-    print(proposals)
+    df_prby = record_output_probabilities(xca, "output_probabilities.csv")
+    proposals = xca.ask(27)
+    proposals = xca.ask(27)
+    df_prop = record_detailed_proposals(xca, "output_proposals.csv")
